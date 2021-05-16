464f7c1be18b39ae63329571de7e277f1e60ecca
hardijabar20 <75072262+hardijabar20@users.noreply.github.com>
2021-05-16 15:24:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /* Your Bot Default Prefix For Every Server */
-exports.Default_Prefix = "";
+exports.Default_Prefix = "$";
 /* Your Bot All Embeds Color */
 exports.Color = "";
 /* Your Bot Token (Secret) */
-exports.Token = "";
+exports.Token = "ODE5MTM4Mzg2NjEwMjI1MTky.YEiQEg.7PpssgUf1lNPsUvbzzRTAMr75No";
 /* Bot's Owner ID */
-exports.OwnerID = "";
+exports.OwnerID = "811564897984577576";
